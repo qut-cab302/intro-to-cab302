0c08b601fcd3ecf6a8cc00769fe899f57e42c57a
Timothy Chappell <timothy.chappell@qut.edu.au>
2022-02-18 08:59:15 +1000

    Add files via upload

---
+++ src/intro/Introduction.java
@@ -0,0 +1,20 @@
+package intro;
+
+/**
+ * Introduction to CAB302: Software Development
+ */
+public class Introduction {
+    public static void main(String[] args) {
+        // Generated with https://patorjk.com/software/taag/
+        System.out.println(
+                " _ _ _       _                           _        \n" +
+                "| | | | ___ | | ___  ___ ._ _ _  ___   _| |_  ___ \n" +
+                "| | | |/ ._>| |/ | '/ . \\| ' ' |/ ._>   | |  / . \\\n" +
+                "|__/_/ \\___.|_|\\_|_.\\___/|_|_|_|\\___.   |_|  \\___/\n" +
+                "    ___  ___  ___  ____ ___  ___                  \n" +
+                "   |  _>| . || . ><__ /|   |<_  >                 \n" +
+                "   | <__|   || . \\ <_ \\| / | / /                  \n" +
+                "   `___/|_|_||___/<___/`___'<___>                 \n" +
+                "                                                  ");
+    }
+}
